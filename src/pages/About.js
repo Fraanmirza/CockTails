170e05e6bd2c9036e08d356b30f979122864346a
Farhan <farhanmirza52@gmail.com>
2023-12-17 21:09:12 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ullam
+        sed tenetur ipsam. Omnis nesciunt necessitatibus, dolor fugiat molestiae
+        excepturi laudantium quibusdam nisi commodi molestias, mollitia quos
+        eius dolorum illo?
+      </p>
+    </section>
+  )
+}
+
+export default About
